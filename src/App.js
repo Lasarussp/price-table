3e5dde0d4426e7b41276d14a7ec832ead9d6a9f5
Lasarussp <001lasarus@gmail.com>
2022-11-02 17:35:35 +0530

    add price card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { PricingTable, PricingSlot, PricingDetail} from "react-pricing-table";
+//install react-pricing-table;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <PricingTable  highlightColor="#1abc9c">
+      <PricingSlot buttonText='TRY IT FREE' title='FREE' priceText='$0/month'>
+        <PricingDetail> ✅ Watch free videos </PricingDetail>
+        <PricingDetail> ❌  With Ad movies </PricingDetail>
+        <PricingDetail> ❌  With Ad videos </PricingDetail>
+        <PricingDetail> ❌  Offline videos </PricingDetail>
+        <PricingDetail> ❌  Buffering videos ! </PricingDetail>
+      </PricingSlot>
+      <PricingSlot buttonText='SIGN UP' title='VIP' priceText='$24/month'>
+        <PricingDetail> ✅  Everything is free </PricingDetail>
+        <PricingDetail> ✅  Ad free movies </PricingDetail>
+        <PricingDetail> ✅  Download available </PricingDetail>
+        <PricingDetail> ❌  Little byte of Ad</PricingDetail>
+        <PricingDetail> ❌  Buffering is too far ! </PricingDetail>
+      </PricingSlot>
+      <PricingSlot buttonText='SIGN UP' title='PRIME' priceText='$64/month'>
+        <PricingDetail> ✅  Everything is free </PricingDetail>
+        <PricingDetail> ✅  Ad free movies videos </PricingDetail>
+        <PricingDetail> ✅  Download available </PricingDetail>
+        <PricingDetail> ✅  Offline available</PricingDetail>
+        <PricingDetail> ✅  No Ad available ! </PricingDetail>
+      </PricingSlot>
+    </PricingTable>
     </div>
   );
 }
